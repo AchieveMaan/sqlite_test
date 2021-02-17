bb8d9dea97f3b00e7e38904def6f5fb9de2281e7
AchieveMaan <72845607+AchieveMaan@users.noreply.github.com>
2021-02-17 13:33:23 +0300

    Add loop test

--- test_bd.cpp
+++ test_bd.cpp
@@ -7,29 +7,31 @@
 using namespace std;
 
 static int callback(void *data, int argc, char**argv, char **azColName){
-    int i;
+//    int i;
 //    cout << (const char*) data << endl;
 
-    for(i =0; i<argc; i++) {
-        cout << azColName[i] << ":[ " << argv[i] << " ]" << endl;
-    }
+//    for(i =0; i<argc; i++) {
+        //cout << azColName[i] << ":[ " << argv[i] << " ]" << endl;
+//    }
     return 0;
 }
 
 int main() {
     sqlite3* db;
-    int res = sqlite3_open("test.db", &db);
-    if(res) 
+    int res = sqlite3_open("cpp.db", &db);
+
+// "test_cpp.db", &db);
+    if(res)
     {
         cout << "Can't open database for reading...\n" << sqlite3_errmsg(db) << endl;
         return res;
     }
     cout << "Database active!" << endl;
 
-    char* command = "CREATE TABLE IF NOT EXISTS students ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, fio TEXT NOT NULL);";
+//    char* command = "CREATE TABLE IF NOT EXISTS students ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, fio TEXT NOT NULL);";
 
-    char* insert = "INSERT INTO students(fio) VALUES (\"SENYA\")";
-    char* select = "SELECT * FROM students";
+//    char* insert = "INSERT INTO students(fio) VALUES (\"SENYA\")";
+//    char* select = "SELECT * FROM students";
 
     char* err = 0;
     cout << fixed;
@@ -38,22 +40,25 @@ int main() {
     double duration;
     start = clock();
 
-    if(sqlite3_exec(db, command, 0, 0, &err)) cout << "error:" << err << endl;
+    sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS students ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, fio TEXT NOT NULL);", 0, 0, &err);
     duration = (clock() - start) / (double) CLOCKS_PER_SEC;
-    cout << "Operation: _" << command << "_ time = [ " << duration << " sec ]" << endl;
+    cout << "Create time = [ " << duration << " sec ]" << endl;
+    sqlite3_free(err);
+
 
     start = clock();
-    if(sqlite3_exec(db, insert, 0, 0, &err)) cout << "error:" << err << endl;
+    for (int i = 0; i < 100; i++) {
+        sqlite3_exec(db, "INSERT INTO students(fio) VALUES (\"SENYA\")", 0, 0, &err);
+    }
     duration = (clock() - start) / (double) CLOCKS_PER_SEC;
-    cout << "Operation: _" << insert << "_ time = [ " << duration << " sec ]" << endl;
-
+    cout << "Insert time = [ " << duration / 100 << " sec ]" << endl;
 
     const char* data = "Callback function called";
     start = clock();
-    if(sqlite3_exec(db, select, callback, (void *) data, &err)) cout << "error:" << err << endl;
+    sqlite3_exec(db, "SELECT * FROM students", callback, (void *) data, &err);
     duration = (clock() - start) / (double) CLOCKS_PER_SEC;
-    cout << "Operation: _" << select << "_ time = [ " << duration << " sec ]" << endl;
-
+    cout << "Select time = [ " << duration << " sec ]" << endl;
+    cout << "-----";
     sqlite3_free(err);
     sqlite3_close(db);
 
--- test_db.py
+++ test_db.py
@@ -1,11 +1,12 @@
 import sqlite3
+import time
 
 from sqlite3 import Error
 
 def main():
 
     try:
-        con = sqlite3.connect("test.db")
+        con = sqlite3.connect("py.db")
         print("Database is active!")
         return con
     except Error:
@@ -15,8 +16,26 @@ con = main()
 
 cursor = con.cursor()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS studentss ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, fio TEXT NOT NULL)")
+start_time = time.time()
+cursor.execute("CREATE TABLE IF NOT EXISTS students ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, fio TEXT NOT NULL)")
 con.commit()
-#    char* insert = "INSERT INTO students(fio) VALUES (\"SENYA\")";
-#    char* select = "SELECT * FROM students";
+print("Create time: [ %.10f sec ]" % (time.time() - start_time))
+
+start_time = time.time()
+for i in range(0, 100):
+    cursor.execute("INSERT INTO students(fio) VALUES (\"SENYA\")")
+    con.commit()
+duration = (time.time() - start_time)
+print("Insert time: [ %.10f sec ]" % (duration / 100))
+
+start_time = time.time()
+#for row in 
+cursor.execute("SELECT * FROM students")
+#    print(row)
+#con.commit()
+print("Select time: [ %.10f sec ]" % (time.time() - start_time))
+
+print("-----")
+#    char* insert = "";
+#    char* select = "";
 con.close()
